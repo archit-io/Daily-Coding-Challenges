63c428ad501d20b2df902ff6553abdbd9142fae2
Archit Choudhary <architc.official@gmail.com>
2022-05-07 18:34:20 +0530

    reverse all the words in a string

---
+++ 7th Kyu/reverseAllWordsInTheSentence.js
@@ -0,0 +1,11 @@
+// reverses each word in the string. All spaces in the string should be retained.
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  return str.split(' ').map((_,i) => _.split('').reverse().join('')).join(' ')
+}
+
+
+
