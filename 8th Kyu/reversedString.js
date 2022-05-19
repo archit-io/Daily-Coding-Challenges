ce29fa871e67ef9c64bdf3fc6d2e26dc9ea5d0ee
Archit Choudhary <architc.official@gmail.com>
2022-05-19 18:14:54 +0530

    reverse the string

---
+++ 8th Kyu/reversedString.js
@@ -0,0 +1,15 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//Answer
+
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
+
+
+// Other solutions
+
+// const solution = s => [...s].reverse().join('')
