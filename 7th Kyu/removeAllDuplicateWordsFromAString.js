ed5076d25690eccb693a4e201b0d12dd724f1b81
Archit Choudhary <architc.official@gmail.com>
2022-07-20 13:21:34 +0530

    remove all duplicate words from a string

---
+++ 7th Kyu/removeAllDuplicateWordsFromAString.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+// Example:
+// Input:
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+// Output:
+// 'alpha beta gamma delta'
+
+function fn(s) {
+            return [...new Set(s.split(' '))].join(' ')            
+        }
