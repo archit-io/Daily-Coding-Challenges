57cc7749d3a031080040a5ed80dad54ed6a85d0f
Archit Choudhary <architc.official@gmail.com>
2022-07-22 17:27:40 +0530

    get flattened and sorted array

---
+++ 7th Kyu/getFlattenedAndSortedArr.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Challenge:
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+// Example:
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+function flattenAndSort(l) {
+  let arr = []
+            for(let i = 0 ; i < l.length ; i++) {
+                for(let j = 0; j < l[i].length ; j++) {
+                    arr.push(l[i][j])
+                }
+            }
+            return arr.sort((a,b) => a-b)
+}
+
+// function flattenAndSort(array) {
+//   return [].concat(...array).sort((a,b) => a - b);
+// }
