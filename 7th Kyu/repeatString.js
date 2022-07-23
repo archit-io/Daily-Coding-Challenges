0c8f9f27f786dc0b82c345f6e14aa1c524f0530e
Archit Choudhary <architc.official@gmail.com>
2022-07-23 11:58:31 +0530

    repeat the string n number of times

---
+++ 7th Kyu/repeatString.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+// Example: (Input1, Input2 --> Output)
+// "a", 5 --> "aaaaa"
+
+function repeater(s, n){
+  return s.repeat(n)
+}
